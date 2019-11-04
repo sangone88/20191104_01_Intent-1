d97e252c03bb85191d98e37bc3230a71bc1fbdc6
kj_cho <kj_cho@idealidea.co.kr>
2019-11-04 19:48:40 +0900

    버튼 눌러서 사용자 정보 수정 화면으로 이동

--- app/src/main/java/com/tjoeun/a20191104_01_intent/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191104_01_intent/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.tjoeun.a20191104_01_intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nameInputBtn.setOnClickListener {
+
+            var intent = Intent(this, EditUserInfoActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
