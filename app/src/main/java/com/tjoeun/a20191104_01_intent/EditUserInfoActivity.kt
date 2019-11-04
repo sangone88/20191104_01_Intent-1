b402d638b234c3e91a2f1c52867697449bffad24
kj_cho <kj_cho@idealidea.co.kr>
2019-11-04 19:45:28 +0900

    이름 입력화면 그리기

---
+++ app/src/main/java/com/tjoeun/a20191104_01_intent/EditUserInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.tjoeun.a20191104_01_intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditUserInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_user_info)
+    }
+}
