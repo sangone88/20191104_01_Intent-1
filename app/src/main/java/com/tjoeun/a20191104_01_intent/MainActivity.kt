932b542ec2546180290bad9eb15ed0e390386892
kj_cho <kj_cho@idealidea.co.kr>
2019-11-04 20:07:39 +0900

    결과로 받아온 String을 화면에 적용

--- app/src/main/java/com/tjoeun/a20191104_01_intent/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191104_01_intent/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tjoeun.a20191104_01_intent
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -19,4 +20,31 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1000) {
+            if (resultCode == Activity.RESULT_OK) {
+
+                var inputNameData = data?.getStringExtra("inputName")
+
+                inputNameData?.let {
+
+                    nameTxt.text = it
+
+                }
+
+            }
+        }
+
+    }
+
 }
+
+
+
+
+
+
+
